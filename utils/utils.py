0e6a4582e9d2567481c306f435af40644dddbcf5
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-08-03 09:02:08 +0530

    model param get function created

--- utils/utils.py
+++ utils/utils.py
@@ -27,3 +27,21 @@ def preprocess_text(text):
     text = text.lower()
     words = re.findall(r'\b\w+\b|[^\w\s]', text)
     return words
+
+
+
+
+def model_params(model):
+    total_params = sum(p.numel() for p in model.parameters())
+    formatted_total_params = "{:,}".format(total_params)
+    print(f"Total parameters: {formatted_total_params}")
+
+
+    trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    formatted_trainable_params = "{:,}".format(trainable_params)
+    print(f"Trainable parameters: {formatted_trainable_params}")
+
+
+    non_trainable_params = total_params - trainable_params
+    formatted_non_trainable_params = "{:,}".format(non_trainable_params)
+    print(f"Non-trainable parameters: {formatted_non_trainable_params}")
